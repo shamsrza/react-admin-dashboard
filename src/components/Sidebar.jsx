784db633a3689bd02f81b183e492d7d85f2938f0
shamsrza <66381642+shamsrza@users.noreply.github.com>
2022-06-21 21:38:54 +0200

    ContextAPI -commit-one

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,9 +10,9 @@ const Sidebar = () => {
   const activeMenu = true;
 
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md hover:bg-gray-100 text-gray-700 dark:text-gray-200 dark:hover:text-black m-2";
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -29,27 +29,32 @@ const Sidebar = () => {
               <button
                 type="button"
                 onClick={() => {}}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 bock md:hidden"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
           </div>
-          <div className="mt-10">
+          <div className="mt-10 ">
             {links.map((item) => (
               <div key={item.title}>
-                <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
+                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
                 {item.links.map((link) => (
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    // onClick={handleCloseSideBar}
+                    // style={({ isActive }) => ({
+                    //   backgroundColor: isActive ? currentColor : '',
+                    // })}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
                   >
                     {link.icon}
-                    <span className="capitalize">{link.name}</span>
+                    <span className="capitalize ">{link.name}</span>
                   </NavLink>
                 ))}
               </div>
