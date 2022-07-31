247c41f72e219bdb20ab0e610b237597a949e331
shamsrza <66381642+shamsrza@users.noreply.github.com>
2022-08-01 01:19:30 +0200

    pages (all)

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -13,8 +13,10 @@ import {
 
 import { scheduleData } from "../data/dummy";
 import { Header } from "../components";
-
+import { useStateContext } from "../contexts/ContextProvider";
 const Calendar = () => {
+  const { currentColor, currentMode } = useStateContext();
+
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Calendar" />
